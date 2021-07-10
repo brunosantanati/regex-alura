971b9c3dd5c132ba61ac8d9488a38a40d0412224
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-07-09 22:29:53 -0300

    Exemplo de como alterar formato da data no C#.

--- exemplos/C#/regex/regex/Program.cs
+++ exemplos/C#/regex/regex/Program.cs
@@ -53,8 +53,31 @@ namespace regex
                 Console.WriteLine();
             }
 
+            alterarFormatoData();
+
             Console.ReadKey();
 
+        }
+
+        private static void alterarFormatoData()
+        {
+            //string alvo = "2007-12-31";
+            string alvo = "2007-12-31 2021-07-09";
+            Regex regexp = new Regex(@"(\d{4})(-)(\d{2})(-)(\d{2})");
+
+            MatchCollection resultados = regexp.Matches(alvo);
+            foreach (Match resultado in resultados)
+            {
+
+                string ano = resultado.Groups[1].Value;
+                string mes = resultado.Groups[3].Value;
+                string dia = resultado.Groups[5].Value;
+
+                string separador1 = resultado.Groups[2].Value;
+                string separador2 = resultado.Groups[4].Value;
+
+                Console.WriteLine(string.Format("{0}/{1}/{2}", dia, mes, ano));
+            }
         }
     }
 }
